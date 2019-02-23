7423d4f596bbf65cee7558bdc81613821342f478
ghamzak <ghka9436@colorado.edu>
2019-02-22 19:40:56 -0700

    slideshow created

---
+++ InteractiveSlideshowApp/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    # path('', views.post_list, name='post_list'),
+    url('^', views.post_list, name='post_list'),
+
+]
